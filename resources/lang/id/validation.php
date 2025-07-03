58ec7b404d01e5c89ddeafabb724c7c820f50906
ASLAM MUZAKY <zakyaslam2004@gmail.com>
2025-07-03 08:08:07 +0700

    7

---
+++ resources/lang/id/validation.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    // ... pesan lain ...
+    'confirmed' => 'Password dan konfirmasi password harus sama.',
+    // ... pesan lain ...
+    'attributes' => [
+        'name' => 'nama',
+        'email' => 'email',
+        'password' => 'password',
+        'password_confirmation' => 'konfirmasi password',
+    ],
+]; 
