da27c540da42516fb959af7696fd6b96d0884137
ASLAM MUZAKY <zakyaslam2004@gmail.com>
2025-07-03 12:03:47 +0700

    7

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -10,9 +10,7 @@ class OrderController extends Controller
     public function index()
     {
         $user = auth()->user();
-        if ($user && $user->role === 'admin') {
-            $orders = Order::orderBy('created_at', 'desc')->get();
-        } else if ($user) {
+        if ($user) {
             $orders = Order::where('user_id', $user->id)->orderBy('created_at', 'desc')->get();
         } else {
             $orders = collect();
